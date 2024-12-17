d2efb08be6c156a841110d01f3117a5b4ee84039
Kepitrion <kepitrion@proton.me>
2024-12-17 06:34:48 +0000

    Custom Errors

--- modules/error.ts
+++ modules/error.ts
@@ -5,3 +5,19 @@ export class DatabaseError extends Error {
     this.name = 'DatabaseError';
   }
 }
+
+export class EngineError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    this.name = 'EngineError';
+  }
+}
+
+export class ParserError extends Error {
+  constructor(message: string) {
+    super(message);
+
+    this.name = 'ParserError';
+  }
+}

57dcd1dea7076497f01f8896d544a5a1b5dd4f20
kuion <kuion10@outlook.com>
2024-12-22 04:24:38 +0000

    More markups implemented & fixed some confusions

--- modules/parser.ts
+++ modules/parser.ts
@@ -6,7 +6,7 @@ class DefaultParser {
   }
 
   parse() {
-    let isInNoMarkup, isInUnderline, isInItalic, isInBold, isInStrikethrough, isInsuperscript, isInsubscript = 0; //0 = False, 1 = Parser Hint, 2 = True
+    let isInNoMarkup, isInUnderline, isInItalic, isInBold, isInStrikethrough, isInsuperscript, isInsubscript = 0; // 0 = False, 1 = Parser Hint, 2 = True
 
     const strList = this.target.split("");
 
@@ -15,9 +15,21 @@ class DefaultParser {
         if(isInBold === 0) isInBold = 1;
         else isInBold = 2;
       }
-      else if (string == "\_") {
+      else if (string == "\'") {
         if(isInItalic === 0) isInItalic = 1;
         else isInItalic = 2;
+      }
+      else if (string == "\_") {
+        if(isInUnderline === 0) isInUnderline = 1;
+        else isInUnderline = 2;
+      }
+      else if (string == "\^") {
+        if(isInsuperscript === 0) isInsuperscript = 1;
+        else isInsuperscript = 2;
+      }
+      else if (string == "\,") {
+        if(isInsubscript === 0) isInsubscript = 1;
+        else isInsubscript = 2;
       }
     }
   }
